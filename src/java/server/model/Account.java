e666b63dcd48c4cffe778f0a8453f31b92b1adb2
Ali.Tavassoly <tavassolyali@gmail.com>
2020-07-11 04:35:54 +0430

    point parameter added

--- src/java/server/model/Account.java
+++ src/java/server/model/Account.java
@@ -6,6 +6,11 @@ public class Account {
     private String username;
     private String password;
 
+    private int totalGames;
+    private int winGames;
+    private int lostGames;
+    private int point;
+
     public Account(int id, String username, String password) {
         this.id = id;
 
@@ -16,7 +21,6 @@ public class Account {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
@@ -24,8 +28,52 @@ public class Account {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTotalGames() {
+        return totalGames;
+    }
+    public void setTotalGames(int totalGames) {
+        this.totalGames = totalGames;
+    }
+
+    public int getWinGames() {
+        return winGames;
+    }
+    public void setWinGames(int winGames) {
+        this.winGames = winGames;
+    }
+
+    public int getLostGames() {
+        return lostGames;
+    }
+    public void setLostGames(int lostGames) {
+        this.lostGames = lostGames;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public void lostGame(){
+        totalGames++;
+        lostGames++;
+    }
+
+    public void wonGame(){
+        totalGames++;
+        winGames++;
     }
 }
